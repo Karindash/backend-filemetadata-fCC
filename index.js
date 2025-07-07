5c0f29b87e816ad4f0fc807aa24622df2e64a071
Karindash <karindashr@gmail.com>
2025-07-07 14:53:36 +0000

    build: adding entire logic to index.js

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
-var express = require('express');
-var cors = require('cors');
 require('dotenv').config()
-
-var app = express();
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+const app = express();
 
 app.use(cors());
+app.use(express.json());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+const upload = multer({ dest: 'uploads/' });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  if (!req.file) {
+    res.status(400).json('No file uploaded.');
+  }
 
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
